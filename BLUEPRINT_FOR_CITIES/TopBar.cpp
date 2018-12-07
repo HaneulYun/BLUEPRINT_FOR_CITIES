fc058cd5bc9c1b3829101f21d187594bb65afee0
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-12-07 18:56:24 +0900

    update bars

--- BLUEPRINT_FOR_CITIES/TopBar.cpp
+++ BLUEPRINT_FOR_CITIES/TopBar.cpp
@@ -14,7 +14,7 @@ void TopBar::initialize()
 	obj.setBMP("resources/menucolor.bmp");
 	obj.setOBJ("resources/topbar.obj");
 	obj.initialize();
-	obj.setPosition(0, float(2.0), 0);
+	obj.setPosition(0, 2.f, 0);
 	obj.setColor(209.f / 255, 178.f / 255, 255.f / 255);
 }
 
