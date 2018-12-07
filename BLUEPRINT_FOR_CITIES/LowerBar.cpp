2ab24bbed232eccfde0de926c90b754eb9c45d7b
minjoo <minf0723@naver.com>
2018-12-07 18:33:05 +0900

    bar can change state

--- BLUEPRINT_FOR_CITIES/LowerBar.cpp
+++ BLUEPRINT_FOR_CITIES/LowerBar.cpp
@@ -1,4 +1,5 @@
 #include "LowerBar.h"
+#include "GameScene.h"
 
 LowerBar::LowerBar()
 {
@@ -13,12 +14,16 @@ void LowerBar::initialize()
 	obj.setBMP("resources/menucolor.bmp");
 	obj.setOBJ("resources/lowerbar.obj");
 	obj.initialize();
-	obj.setPosition(0, -float(0.82), 0);
+	obj.setPosition(0, -float(2.0), 0);
 }
 
 void LowerBar::update()
 {
 	obj.update();
+	if (g_gameScene->viewMode)
+		obj.setPosition(0, -float(2.0), 0);
+	else
+		obj.setPosition(0, -float(0.82), 0);
 }
 
 void LowerBar::render()
--- BLUEPRINT_FOR_CITIES/TopBar.cpp
+++ BLUEPRINT_FOR_CITIES/TopBar.cpp
@@ -1,4 +1,5 @@
 #include "TopBar.h"
+#include "GameScene.h"
 
 TopBar::TopBar()
 {
@@ -13,12 +14,16 @@ void TopBar::initialize()
 	obj.setBMP("resources/menucolor.bmp");
 	obj.setOBJ("resources/topbar.obj");
 	obj.initialize();
-	obj.setPosition(0, float(0.95), 0);
+	obj.setPosition(0, float(2.0), 0);
 }
 
 void TopBar::update()
 {
 	obj.update();
+	if (g_gameScene->viewMode)
+		obj.setPosition(0, float(2.0), 0);
+	else
+		obj.setPosition(0, float(0.92), 0);
 }
 
 void TopBar::render()
