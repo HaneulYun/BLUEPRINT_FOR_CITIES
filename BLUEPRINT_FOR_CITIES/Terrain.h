daa6e11652cef1c5ef2faf7352f50c3caa3718cf
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-12-07 22:46:44 +0900

    add ray-obb collision detection

--- BLUEPRINT_FOR_CITIES/Terrain.h
+++ BLUEPRINT_FOR_CITIES/Terrain.h
@@ -38,7 +38,6 @@ public:
 	Terrain();
 	~Terrain();
 
-
 	void initialize();
 	void update();
 	void render();
---
+++ BLUEPRINT_FOR_CITIES/picking.h
@@ -0,0 +1,3 @@
+#pragma once
+
+bool TestRayOBBIntersection(glm::vec3 ray_origin, glm::vec3 ray_direction, glm::vec3 aabb_min, glm::vec3 aabb_max, glm::mat4 ModelMatrix, float& intersection_distance);
